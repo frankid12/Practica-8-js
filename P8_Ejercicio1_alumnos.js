8998c4a5ca253a2eb9ecaa40eb527aa4555ed479
fruizotero <frank.git96@gmail.com>
2023-12-01 18:48:51 +0100

    Función crearTextoConEstilos - Crea y añade estilos

--- P8_Ejercicio1_alumnos.js
+++ P8_Ejercicio1_alumnos.js
@@ -1,11 +1,55 @@
+
+
 const d = document;
-function crearTexto(params) {
+function crearTexto() {
     let formulario = d.getElementById("miFormulario");
+    let resultado = d.getElementById("resultado");
     let nombre = formulario["nombre"].value;
     let color = formulario["color"].value;
     let fuente = formulario["fuente"].value;
-    // let color = d.getElementById("color").value.toLowerCase();
-    // let fuente = d.getElementById("fuente").value.toLowerCase();
-    console.log(formulario["negrita"].checked);
+    let tachado = formulario["tachado"].checked;
+    let negrita = formulario["negrita"].checked;
+    let cursiva = formulario["cursiva"].checked;
+    let versalita = formulario["versalita"].checked;
+    let subrayado = formulario["subrayado"].checked;
+
+    let textConEstilos = crearTextoConEstilos(nombre, color, fuente, tachado, negrita, cursiva, versalita, subrayado);
+
+    resultado.replaceChildren(textConEstilos);
+
+    let opciones = "left=100,top=100,width=250,height=300";
+    let ventana = window.open("", "", opciones);
+
+    let h1 = d.createElement("h1");
+    h1.innerText = nombre;
+
+    let hr = d.createElement("hr");
+    let button = d.createElement("button");
+    button.innerText = "cerrar";
+    button.addEventListener("click", () => ventana.close());
+
+    ventana.document.body.appendChild(h1);
+    ventana.document.body.appendChild(hr);
+    ventana.document.body.appendChild(textConEstilos);
+    ventana.document.body.appendChild(button);
+
 }
+
+function crearTextoConEstilos(nombre, color, fuente, tachado, negrita, cursiva, versalita, subrayado) {
+    let texto = document.createElement("p");
+    texto.innerText = nombre;
+
+    texto.style.textTransform = "capitalize";
+    texto.style.color = color;
+    texto.style.fontFamily = fuente;
+    texto.style.fontSize = "3rem"; // 48px
+    texto.style.textDecoration = tachado && "line-through";
+    texto.style.fontWeight = negrita && "bold";
+    texto.style.fontStyle = cursiva && "italic";
+    texto.style.fontVariant = versalita && "small-caps";
+    texto.style.textDecoration = subrayado && "underline";
+
+    return texto;
+}
+
 
